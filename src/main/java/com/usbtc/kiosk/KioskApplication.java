fea55e55771d43e8f46b4d0fb5c2605efbc59068
Odohi David <odohi.david@gmail.com>
2019-12-18 11:26:57 +0100

    add enableautoconfiguration for the application

--- src/main/java/com/usbtc/kiosk/KioskApplication.java
+++ src/main/java/com/usbtc/kiosk/KioskApplication.java
@@ -1,20 +1,15 @@
 package com.usbtc.kiosk;
 
-import com.usbtc.kiosk.configurations.ApiConfiguration;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class KioskApplication {
-
-	@Autowired
-	private ApiConfiguration apiConfiguration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KioskApplication.class, args);
-		// System.out.println(apiConfiguration.getUrl());
 	}
 
 }
