091c575281077ab3aa66eee41136021d4fbaf779
Odohi David <odohi.david@gmail.com>
2019-12-18 11:27:58 +0100

    create appautoconfiguration

---
+++ src/main/java/com/usbtc/kiosk/configurations/AppAutoConfiguration.java
@@ -0,0 +1,21 @@
+package com.usbtc.kiosk.configurations;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AppAutoConfiguration
+ */
+@Configuration
+@ConditionalOnProperty(name = "api.spankie.mylog", havingValue = "true")
+public class AppAutoConfiguration {
+
+  @Bean
+  public FilterRegistrationBean<LogFilter> myLogFilter() {
+    FilterRegistrationBean<LogFilter> frb = new FilterRegistrationBean<>();
+    frb.setFilter(new LogFilter());
+    return frb;
+  }
+}
