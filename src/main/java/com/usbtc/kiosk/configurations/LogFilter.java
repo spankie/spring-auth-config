b6b95442f3f64259e664cc817b77eab66d74c2f9
Odohi David <odohi.david@gmail.com>
2019-12-18 11:28:48 +0100

    create logfilter

---
+++ src/main/java/com/usbtc/kiosk/configurations/LogFilter.java
@@ -0,0 +1,30 @@
+package com.usbtc.kiosk.configurations;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * LogFilter
+ */
+public class LogFilter extends OncePerRequestFilter {
+
+  private Logger log = LoggerFactory.getLogger(LogFilter.class);
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    log.debug("Hi there... contentType: ");
+    System.out.println("hi there... what is happengin" + request.getRemotePort());
+    response.setStatus(201);
+    filterChain.doFilter(request, response);
+  }
+
+}
