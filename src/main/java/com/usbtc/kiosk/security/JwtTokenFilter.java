b15d1b4aa8be991ade858eb85bdbba8c87abf778
Odohi David <odohi.david@gmail.com>
2020-02-17 23:36:10 +0100

    catch the right exception and return after handling the exception

--- src/main/java/com/usbtc/kiosk/security/JwtTokenFilter.java
+++ src/main/java/com/usbtc/kiosk/security/JwtTokenFilter.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.usbtc.kiosk.exceptions.CustomException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -28,19 +30,21 @@ public class JwtTokenFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+    // Removes the "Bearer " from the authorization header
     String token = jwtTokenProvider.resolveToken(request);
     try {
       if (token != null && jwtTokenProvider.validateToken(token)) {
         Authentication auth = jwtTokenProvider.getAuthentication(token);
         SecurityContextHolder.getContext().setAuthentication(auth);
       }
-    } catch (CustomException e) {
+    } catch (UsernameNotFoundException e) {
       // this is very important, since it guarantees the user is not authenticated at
       // all
       SecurityContextHolder.clearContext();
       // see what happens if this error is not sent
-      response.sendError(e.getStatus().value(), e.getMessage());
+      response.sendError(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+      // OR just throw a custom exception that would be caught by exception handler
+      return; // dont call the rest of the filter chain
     }
 
     filterChain.doFilter(request, response);
